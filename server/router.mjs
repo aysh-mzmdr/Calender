14621f677479f46f28c24d2a6226470698fad0d9
aysh-mzmdr <ayush.mazumdar111@gmail.com>
2026-04-15 12:12:22 +0530

    Fixed date data

--- server/router.mjs
+++ server/router.mjs
@@ -7,15 +7,16 @@ router.post("/api/update", async (request,response) => {
     const {date,color,note} = request.body
     try{
         await pool.query(`INSERT INTO dates (date,color,note) VALUES ($1,$2,$3) ON CONFLICT (date) DO UPDATE SET color = $2, note = $3`,[date,color,note])
-        return response.send(201)
+        return response.sendStatus(201)
     }
     catch(err){
-        return response(err)
+        console.log(err)
+        return response.sendStatus(500)
     }
 })
 
 router.get("/api/getDates",async(request,response) => {
-    const data = await pool.query("SELECT * FROM dates")
+    const data = await pool.query("SELECT TO_CHAR(date,'YYYY-MM-DD') AS date,color,note FROM dates")
     console.log(data.rows)
     const dateData = data.rows
     response.send(dateData)
